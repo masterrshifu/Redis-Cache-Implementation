de15a96824f771b45e7ff0effa70c2ceadef5662
akshay agarwal <programmingwithakshay@gmail.com>
2023-08-07 00:54:48 +0530

    redis example

---
+++ spring-data-redis-example/src/main/java/com/redis/sprinboot/SpringDataRedisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.redis.sprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+	}
+
+}
